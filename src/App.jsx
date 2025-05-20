d65ba77dec5506546ce884de8e4cfd3a477e534c
baisakhi-aich <aichbaisakhi30@gmail.com>
2025-05-20 17:45:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Footer from './Component/Footer/Footer'
 import Price from './Component/Price/Price'
 import Home from './Component/Home/Home'
 import Steps from './Component/Steps/Steps'
+import Vdopage from './Component/Vdopage/Vdopage'
 const App = () => {
   return (
     <div>
     <Header/>
  <Home/>
 <Steps/>
+<Vdopage/>
     <Price/>
     <Footer/>
     </div>
